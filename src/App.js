fb44b2978f1abfe426c7ee74edcd42d9c065cc9b
gdcash <74430058+gdcash@users.noreply.github.com>
2021-04-05 17:22:58 -0500

    [NEW] Update management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./Redux";
+import getDigitalService from "./service/digitalServices";
 
-function App() {
-  return <View></View>;
-}
+const App = () => {
+  getDigitalService().then((res) => console.log(res));
+  return <Provider store={store}> </Provider>;
+};
 
 export default App;
---
+++ src/Redux/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./rootReducer";
+import rootSaga from "./rootSaga";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer(),
+  compose(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
