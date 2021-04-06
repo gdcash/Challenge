d51a29621cd06f8a452a68449dd0965450481188
gdcash <74430058+gdcash@users.noreply.github.com>
2021-04-06 12:45:12 -0500

    [CASH] many

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// *** TEST INSTRUCTIONS ***
+// Create the page shown of digital service groups and a list of items that is changed whenever a digital service group is tapped.
+// The list items do not need to be tappable for this exercise.
+// Use the build in digital services "API" to fetch the required information to build the page.
+// Within the API response you can find the images for each icon, as well as all of the properties/labels necessary to complete the page.
+// Ask questions as necessary and feel free to look up things on Google.
+//Don't worry about perfect styling or finishing the entire exercise before time expires.
 import React from "react";
 import { View } from "react-native";
 import { Provider } from "react-redux";
@@ -6,7 +13,7 @@ import getDigitalService from "./service/digitalServices";
 
 const App = () => {
   getDigitalService().then((res) => console.log(res));
-  return <Provider store={store}> </Provider>;
+  return <>Hola Cash 💰</>;
 };
 
 export default App;
